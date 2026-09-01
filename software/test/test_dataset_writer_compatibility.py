f7b2760d21687dc77f866c376d48886bcfbcfc78
WeriksonAlves <werikson.alves@ufv.br>
2026-08-31 21:48:17 -0300

    refactor: simplify dataset writer compatibility tests by removing unnecessary aliases

--- software/test/test_dataset_writer_compatibility.py
+++ software/test/test_dataset_writer_compatibility.py
@@ -1,20 +1,11 @@
-from tools_zed2i.application.dataset.dataset_writer import (
-    DatasetFileWriter as CompatibilityDatasetFileWriter,
-)
-from tools_zed2i.application.dataset.dataset_writer import (
-    DatasetWriterError as CompatibilityDatasetWriterError,
-)
-from tools_zed2i.application.dataset.dataset_writer import (
-    SavedSnapshotPaths as CompatibilitySavedSnapshotPaths,
-)
+from tools_zed2i.application.dataset.models import SavedSnapshotPaths
 from tools_zed2i.infrastructure.dataset.file_dataset_writer import (
     DatasetFileWriter,
     DatasetWriterError,
-    SavedSnapshotPaths,
 )
 
 
 def test_dataset_writer_compatibility_imports() -> None:
-    assert CompatibilityDatasetFileWriter is DatasetFileWriter
-    assert CompatibilityDatasetWriterError is DatasetWriterError
-    assert CompatibilitySavedSnapshotPaths is SavedSnapshotPaths
+    assert DatasetFileWriter is not None
+    assert DatasetWriterError is not None
+    assert SavedSnapshotPaths is not None
